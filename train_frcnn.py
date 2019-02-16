eb820bcc7b791a4a139adb8a434a850206b38d53
Dimetrix <d_maskaev@rambler.ru>
2019-02-16 22:58:09 +0300

    Initial

--- train_frcnn.py
+++ train_frcnn.py
@@ -147,7 +147,7 @@ model_rpn.compile(optimizer=optimizer, loss=[losses.rpn_loss_cls(num_anchors), l
 model_classifier.compile(optimizer=optimizer_classifier, loss=[losses.class_loss_cls, losses.class_loss_regr(len(classes_count)-1)], metrics={'dense_class_{}'.format(len(classes_count)): 'accuracy'})
 model_all.compile(optimizer='sgd', loss='mae')
 
-epoch_length = 20
+epoch_length = 500
 num_epochs = int(options.num_epochs)
 iter_num = 0
 
